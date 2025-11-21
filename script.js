18c4b05e9b08c52646d56c2ceabd703ea33155da
aicha_elya <aichaelyadri60@gmail.com>
2025-11-21 15:13:25 +0100

    les fonctions pour afficher et supprimer  modal

---
+++ script.js
@@ -0,0 +1,6 @@
+function ajout() {
+  document.querySelector(".formmodal").classList.remove("closes");
+}
+function closemodal() {
+  document.querySelector(".formmodal").classList.add("closes");
+}
